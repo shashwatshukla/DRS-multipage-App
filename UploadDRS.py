1504cb022f3c2cabc279755b6a032175a28ed342
Manish Srivastava <onemanish@gmx.com>
2022-04-07 23:49:22 +0900

    Removed collapsible container for upload. Checks for ZZZ at the end of imported rows to confirm valid DRS file Small other changes

--- UploadDRS.py
+++ UploadDRS.py
@@ -4,21 +4,19 @@ import sqlite3
 
 
 def upload_drs():
-    upldSection = st.expander('Upload vessel DR Sender')
     df = get_data('mms_master.sqlite', 'drsend')
     drsHeaders = df.columns.values
-    ucol1, ucol2, ucol3 = upldSection.columns(3)
-    with upldSection:
-        uploaded_file = st.file_uploader('Choose a file', type=['xlsm'])
-        if uploaded_file is not None:
-            dfVslDrs = pd.read_excel(uploaded_file, sheet_name='DRSEND', skiprows=6, dtype=str,
-                                     na_filter=False, parse_dates=False, usecols='A:CV')
-            # import data from excel with all col=str and do not put <NA> for missing data
-            filename = uploaded_file.name
+    uploaded_file = st.file_uploader('Upload an updated DR Sender file here.', type=['xlsm'])
+    if uploaded_file is not None:
+        dfVslDrs = pd.read_excel(uploaded_file, sheet_name='DRSEND', skiprows=6, dtype=str,
+                                 na_filter=False, parse_dates=False, usecols='A:CV')
+        # import data from excel with all col=str and do not put <NA> for missing data
+        filename = uploaded_file.name
+        if dfVslDrs.iloc[-1, 0] == "ZZZ":  # implemented crude check for a valid DRS file
             dfVslDrs.drop(dfVslDrs.index[-1], inplace=True)  # drop the last row - with ZZZ
             vsldfShape = dfVslDrs.shape
-            st.write('Raw data from Vessel:', (vsldfShape[0]), 'Records found in "',
-                     filename, '" (', vsldfShape[1], 'Columns)')
+            st.markdown(f'Raw data from Vessel: \n{vsldfShape[0]} Records found in {filename}, '
+                     f'(in {vsldfShape[1]} Columns)')
             dfVslDrs.columns = drsHeaders  # rename the headers for Vessel file, same as master db
             toCorrect = ['dt_ocurred', 'init_action_ship_dt', 'target_dt', 'final_action_ship_dt', 'done_dt',
                          'update_dt']
@@ -29,7 +27,10 @@ def upload_drs():
             dfNoCommon = df[~df['DRS_ID'].isin(drsID)]  # filter OUT all rows with common DRS_ID
             st.write(len(df[df['DRS_ID'].isin(drsID)]), "common items found and updated with latest info.", )
             dfUpdated = pd.concat([dfNoCommon, dfVslDrs], ignore_index=True)  # add all the new rows to dataframe
-            st.dataframe(dfVslDrs)  # diplay DF
+            st.dataframe(dfVslDrs)  # display DF
+
             conn = sqlite3.connect('new.sqlite')  # write complete data to new database for check
             dfUpdated.to_sql('drsend', conn, if_exists='replace', index=False)
             conn.close()
+        else:
+            st.warning('Uploaded File is not a valid DR Sender file. \nPlease try again!')
